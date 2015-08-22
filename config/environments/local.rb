07088607c634ac5396d9e60b7def4bd33f404cc1
Corey Psoinos <cpsoinos@Coreys-MacBook-Air.local>
2015-08-21 23:39:37 -0400

    use lograge for logging

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # config.logger = Logger.new(STDOUT)
+  config.lograge.logger = ActiveSupport::Logger.new(STDOUT)
 end
