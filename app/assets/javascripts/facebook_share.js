74be49ec2808ff49deaa378fba9d2f888465592a
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-05 11:17:40 -0400

    clear fields when switching share type

--- app/assets/javascripts/facebook_share.js
+++ app/assets/javascripts/facebook_share.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   $("input[name=options]").on("change", function() {
+    // first, set the share type
     var shareType = "." + this.id;
+    // second, hide all but text field types
+    $(".share-field").hide();
+    // third, show the selected field type
     $(shareType).show();
-    if(this.id === "text-share") {
-      $(".link-share").hide();
+
+    // fourth, empty the photo or link fields if different share type selected
+    if(shareType === ".text-share") {
+      $("#facebook_share_photo").val("");
+      $("#facebook_share_link").val("");
+    } else if(shareType === ".link-share") {
+      $("#facebook_share_photo").val("");
+    } else if(shareType === ".photo-share") {
+      $("#facebook_share_link").val("");
     }
   });
 });
