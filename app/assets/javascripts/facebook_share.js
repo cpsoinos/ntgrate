8ee9f5eb49cf0d7fb7e3aa549784c8410276f508
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-05 13:36:56 -0400

    client-side validation for fb media uploads

--- app/assets/javascripts/facebook_share.js
+++ app/assets/javascripts/facebook_share.js
@@ -30,3 +30,79 @@ $(document).ready(function() {
     }
   });
 });
+
+function validateFiles(inputFile) {
+  var maxExceededMessage;
+  var extErrorMessage;
+  var allowedExtension;
+  var shareType = $("input[name=options]:checked").attr("id");
+
+  if (shareType === "photo-share") {
+    maxExceededMessage = "This file exceeds the maximum allowed file size (5 MB). ";
+    extErrorMessage = "Only image files with extension: .jpg, .jpeg, .gif or .png are allowed.";
+    allowedExtension = ["jpg", "jpeg", "gif", "png"];
+  } else if(shareType === "video-share") {
+    maxExceededMessage = "This file exceeds the maximum allowed file size (500 MB). ";
+    extErrorMessage = "Only video files with extension: .3g2, .3gp, .3gpp, .asf, .avi, .dat, .divx, .dv, .f4v, .flv, .m2ts, .m4v, .mkv, .mod, .mov, .mp4, .mpe, .mpeg, .mpeg4, .mpg , .mts, .nsv, .ogm, .ogv, .qt, .tod, .ts, .vob, or .wm are allowed.";
+    allowedExtension = [
+      "3g2",
+      "3gp",
+      "3gpp",
+      "asf",
+      "avi",
+      "dat",
+      "divx",
+      "dv",
+      "f4v",
+      "flv",
+      "m2ts",
+      "m4v",
+      "mkv",
+      "mod",
+      "mov",
+      "mp4",
+      "mpe ",
+      "mpeg ",
+      "mpeg4",
+      "mpg ",
+      "mts",
+      "nsv",
+      "ogm",
+      "ogv",
+      "qt",
+      "tod",
+      "ts",
+      "vob",
+      "wmv"
+    ];
+  }
+
+  var extName;
+  var maxFileSize = $(inputFile).data('max-file-size');
+  var sizeExceeded = false;
+  var extError = false;
+
+  $.each(inputFile.files, function() {
+    if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {
+      sizeExceeded = true;
+    }
+    extName = this.name.split('.').pop();
+    if ($.inArray(extName, allowedExtension) == -1) {extError=true;}
+  });
+  if (sizeExceeded) {
+    $("#facebook-errors").show();
+    $("#facebook-errors").append(maxExceededMessage);
+    $(inputFile).val('');
+  }
+
+  if (extError) {
+    $("#facebook-errors").show();
+    $("#facebook-errors").append(extErrorMessage);
+    $(inputFile).val('');
+  }
+}
+
+// set facebook error div to fade out after 15 sec
+$(document).ready( function() {
+  $('#facebook-errors').delay(15000).fadeOut();
+});
