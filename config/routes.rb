33562107ad0068ed5c27c69a013fcc92fc7b73f5
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-12 10:42:40 -0400

    format devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    :omniauth_callbacks => 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords'
   }
   devise_scope :user do
     # delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
