beb96cd251410c7d0633a5db9b51cbe102d181d1
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-13 10:52:20 -0400

    add coverage for user methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,25 @@ describe User do
 
     expect(user.identities.first.provider).to eq("facebook")
   end
+
+  it "validates full name" do
+    expect(user.full_name).to eq("#{user.first_name} #{user.last_name}")
+  end
+end
+
+describe "#email_verified?" do
+  let!(:user) { create(:user) }
+
+  it "returns true when verified" do
+    expect(user.email_verified?).to be(true)
+  end
+
+  it "returns false when unverified" do
+    user.update_attribute("email", "change@me.facebook.com")
+
+    expect(user.email_verified?).to be(false)
+  end
+
 end
 
 describe "#facebook_account" do
@@ -32,6 +51,33 @@ describe "#facebook_account" do
     facebook_account = create(:facebook_account, identity: identity)
 
     expect(user.facebook_account).to eq(facebook_account)
+  end
+end
+
+describe "#facebook_pages" do
+  let(:facebook_page) { create(:facebook_page) }
+  let(:user) { facebook_page.facebook_account.identity.user }
+  let(:user2) { create(:user) }
+
+  it "returns nil when no Facebook account" do
+    expect(user2.facebook_pages).to eq(nil)
+  end
+
+  it "returns an empty array when Facebook account present but has no pages" do
+    facebook_account = create(:facebook_account)
+    user3 = facebook_account.identity.user
+
+    expect(user3.facebook_pages).to eq([])
+  end
+
+  it "returns Facebook pages when present" do
+    expect(user.facebook_pages.count).to be(1)
+  end
+
+  it "can have more than one Facebook page" do
+    create(:facebook_page, facebook_account: user.facebook_account)
+
+    expect(user.facebook_pages.count).to be(2)
   end
 end
 
@@ -63,4 +109,21 @@ describe "#instagram_account" do
 
     expect(user.instagram_account).to eq(instagram_account)
   end
+end
+
+describe "#linkedin_account" do
+  let(:user) { create(:user) }
+
+  it "returns nil when no LinkedIn account" do
+    expect(user.linkedin_account).to eq(nil)
+  end
+
+  it "returns LinkedIn account when connected" do
+    pending("LinkedIn")
+    identity = create(:identity, :linkedin, user: user)
+    linkedin_account = create(:linkedin_account, identity: identity)
+
+    expect(user.linkedin_account).to eq(linkedin_account)
+  end
+
 end
