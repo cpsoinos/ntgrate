b156e8d097fea6541d74a77132e23a483fcae231
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-13 14:41:41 -0400

    start testing fb shares

--- spec/models/facebook_share_spec.rb
+++ spec/models/facebook_share_spec.rb
@@ -9,11 +9,18 @@ describe FacebookShare do
 end
 
 describe FacebookShare, "#share" do
-  it "shares to a Facebook page" do
-    pending("testing with apis")
-    facebook_share = create(:facebook_share)
 
-    expect(facebook_share.share.response).to eq("success")
+  let(:user) { create_from_omniauth }
+  # let(:facebook_share) { create(:facebook_share) }
+
+  it "shares to a Facebook page" do
+    facebook_share = user.facebook_pages.first.facebook_shares.new(content: "this is a test")
+  # expect(Object.method :is_a?).to be_delayed(Object)
+    facebook_share.share
+    binding.pry
+    expect(FacebookShareJob).to have(1).enqueued.job
+    # expect(facebook_share.share).to be_delayed(FacebookShareJob)
+    # expect(facebook_share.share).to change(FacebookShareJob.jobs, :size).by(1)
   end
 end
 
