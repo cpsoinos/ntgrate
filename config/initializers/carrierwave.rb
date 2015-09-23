c798c36ede51aa6f314c95b7f85be2c93eadc793
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-23 09:14:22 -0400

    remove commented unused code

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,6 @@ if Rails.env.test?
 end
 
 CarrierWave.configure do |config|
-  # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
