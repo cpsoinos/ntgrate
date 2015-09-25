75923c9a2e82756eefed0adce071f7ad2164a212
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-24 20:39:39 -0400

    use jquery-readyselector to set page specific js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.remotipart
+//= require jquery-readyselector
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(document).ready( function() {
+$(document).ready(function() {
   $('.alert').delay(5000).fadeOut();
 });
