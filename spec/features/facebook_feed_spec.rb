c34938932b6567b753b71a5db46dcbb5e7c3c2d8
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-24 20:09:22 -0400

    configure parallel tests

--- spec/features/facebook_feed_spec.rb
+++ spec/features/facebook_feed_spec.rb
@@ -7,22 +7,22 @@ feature "facebook feed" do
     sign_in(user)
   end
 
-  # describe "FacebookNewsFeed" do
-  #   it "shows the 5 most recent posts", :js, :vcr, record: :new_episodes do
-  #     pending("ajax load testing")
-  #     visit dashboard_path(user.dashboard)
-  #     find("#show-newsfeed").trigger("click")
-  #
-  #     expect(page).to have_link("Tester Realty")
-  #   end
-  #
-  #   it "has a 'boost' button", :js, :vcr, record: :new_episodes do
-  #     pending("ajax load testing")
-  #     visit dashboard_path(user.dashboard)
-  #     find("#show-newsfeed").trigger("click")
-  #
-  #     expect(page).to have_button("Boost")
-  #   end
-  # end
+  describe "FacebookNewsFeed", :js, :vcr, record: :new_episodes do
+    it "shows the 5 most recent posts", :js, :vcr, record: :new_episodes do
+      pending("ajax load testing")
+      visit dashboard_path(user.dashboard)
+      find("#show-newsfeed").trigger("click")
+
+      expect(page).to have_link("Tester Realty")
+    end
+
+    it "has a 'boost' button", :js, :vcr, record: :new_episodes do
+      pending("ajax load testing")
+      visit dashboard_path(user.dashboard)
+      find("#show-newsfeed").trigger("click")
+
+      expect(page).to have_button("Boost")
+    end
+  end
 
 end
