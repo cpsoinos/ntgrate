0956320ed81cb4cd47e381a615e9d3208c57c40c
Corey Psoinos <coreypsoinos@gmail.com>
2015-09-26 14:46:39 -0400

    pass page info as json for ddslick

--- app/controllers/facebook_accounts_controller.rb
+++ app/controllers/facebook_accounts_controller.rb
@@ -4,6 +4,21 @@ class FacebookAccountsController < ApplicationController
   def show
     @facebook_account = FacebookAccount.find(params[:id])
     @facebook_pages = @facebook_account.try(:facebook_pages)
+    gon.facebook_pages = build_json_for_pages
+  end
+
+  private
+
+  def build_json_for_pages
+    @facebook_pages.map do |page|
+      {
+        text: page.name,
+        value: page.id,
+        selected: false,
+        description: page.category,
+        imageSrc: page.picture
+      }
+    end.to_json
   end
 
 end
