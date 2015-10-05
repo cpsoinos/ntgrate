c1fc84b0946073c6000d926b575b0cb8351235a2
Corey Psoinos <coreypsoinos@gmail.com>
2015-10-04 22:24:51 -0400

    comment out fb feed spec

--- spec/features/facebook_feed_spec.rb
+++ spec/features/facebook_feed_spec.rb
@@ -1,26 +1,26 @@
-feature "facebook feed" do
-
-  let(:user) { create_from_omniauth }
-
-  before (:each) do
-    user.confirm
-    sign_in(user)
-  end
-
-  # describe "FacebookNewsFeed", :js, :vcr, record: :new_episodes do
-  #   it "shows the 5 most recent posts", :js, :vcr, record: :new_episodes do
-  #     visit dashboard_path(user.dashboard)
-  #     find("#show-newsfeed").trigger("click")
-  #
-  #     expect(page).to have_link("Tester Realty")
-  #   end
-
-    # it "has a 'boost' button", :js, :vcr, record: :new_episodes do
-    #   visit dashboard_path(user.dashboard)
-    #   find("#show-newsfeed").trigger("click")
-    #
-    #   expect(page).to have_button("Boost")
-    # end
-  end
-
-end
+# feature "facebook feed" do
+#
+#   let(:user) { create_from_omniauth }
+#
+#   before (:each) do
+#     user.confirm
+#     sign_in(user)
+#   end
+#
+#   describe "FacebookNewsFeed", :js, :vcr, record: :new_episodes do
+#     it "shows the 5 most recent posts", :js, :vcr, record: :new_episodes do
+#       visit dashboard_path(user.dashboard)
+#       find("#show-newsfeed").trigger("click")
+#
+#       expect(page).to have_link("Tester Realty")
+#     end
+#
+#     it "has a 'boost' button", :js, :vcr, record: :new_episodes do
+#       visit dashboard_path(user.dashboard)
+#       find("#show-newsfeed").trigger("click")
+#
+#       expect(page).to have_button("Boost")
+#     end
+#   end
+#
+# end
