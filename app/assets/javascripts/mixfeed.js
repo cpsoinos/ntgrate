73ac1c8a39693279187fa2ed03eea1959f80217e
Corey Psoinos <coreypsoinos@gmail.com>
2015-10-11 21:57:50 -0400

    twitter share actions on mixfeed

--- app/assets/javascripts/mixfeed.js
+++ app/assets/javascripts/mixfeed.js
@@ -5,4 +5,32 @@ $(".mixfeeds.show").ready(function() {
   $grid.imagesLoaded( function() {
     $grid.masonry();
   });
+
+  $(".tweet-reply").click(function(ev) {
+    ev.preventDefault();
+    var tweetId = ("#" + $(this).attr("data"));
+    var replyForm = tweetId + "-reply-form"
+    var replyField = tweetId + "-reply-field"
+    var replyButton = tweetId + "-reply-button"
+    var charsMessage = tweetId + "-message"
+    debugger;
+    $(replyForm).show();
+    $(".grid").masonry();
+
+    $("#tweet").attr("disabled", "disabled");
+    $(replyField).keyup(function(){
+      var chars = $(this).val().length;
+      $(charsMessage).text(140 - chars);
+
+      if(chars > 140 || chars <= 0) {
+        $(replyButton).attr("disabled", "disabled");
+        $(charsMessage).addClass("minus");
+
+      } else {
+        $(replyButton).removeAttr("disabled");
+        $(charsMessage).removeClass("minus");
+      }
+    });
+  })
+
 });
--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,11 @@ Rails.application.routes.draw do
   get '/mixfeeds/:user_id/retrieve_feeds', to: 'mixfeeds#retrieve_feeds'
   get '/mixfeeds/:user_id/insert_feeds', to: 'mixfeeds#insert_feeds'
 
+  post '/twitter_shares/retweet/:tweet_uid', as: "twitter_shares_retweet", to: 'twitter_shares#retweet'
+  post '/twitter_shares/favorite/:tweet_uid', as: "twitter_shares_favorite", to: 'twitter_shares#favorite'
+  post '/twitter_shares/unfavorite/:tweet_uid', as: "twitter_shares_unfavorite", to: 'twitter_shares#unfavorite'
+  post '/twitter_shares/reply/:tweet_uid', as: "twitter_shares_reply", to: 'twitter_shares#reply'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
